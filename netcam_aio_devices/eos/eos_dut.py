636216d3ba8adf5a502b09d4e3ace2db4d01fa3b
Jeremy Schulman <jeremy.schulman@mlb.com>
2021-11-21 15:08:26 -0500

    update

--- netcam_aio_devices/eos/eos_dut.py
+++ netcam_aio_devices/eos/eos_dut.py
@@ -31,34 +31,63 @@ __all__ = ["DeviceUnderTestEOS"]
 
 
 class DeviceEAPIAuth(DeviceEAPI):
+    """define a class that uses environment variables for EAPI auth acccess."""
+
     auth = httpx.BasicAuth(
         username=os.environ["NETWORK_USERNAME"], password=os.environ["NETWORK_PASSWORD"]
     )
 
 
 class DeviceUnderTestEOS(AsyncDeviceUnderTest):
+    """
+    This class provides the Arista EOS device-under-test plugin for directly
+    communicating with the device via the EAPI interface.  The underpinning
+    transport is using asyncio.  Refer to the `aioeapi` distribution for further
+    details.
+    """
+
     def __init__(self, device: Device, **_kwargs):
+        """DUT construction creates instance of EAPI transport"""
         super().__init__(device=device)
         self.eapi = DeviceEAPIAuth(host=device.name)
         self.version_info: Optional[dict] = None
 
     async def setup(self):
+        """DUT setup process"""
         self.version_info = await self.eapi.cli("show version")
 
     async def teardown(self):
+        """DUT tearndown process"""
         await self.eapi.aclose()
 
     @singledispatchmethod
     async def execute_testcases(self, testcases: TestCases):
+        """dispatch the testcases to the registered methods"""
         cls_name = testcases.__class__.__name__
         raise RuntimeError(
             f'Missing: device {self.device.name} support for testcases of type "{cls_name}"'
         )
 
+    # -------------------------------------------------------------------------
+    # Support the 'device' testcases
+    # -------------------------------------------------------------------------
+
     from .eos_tc_device_info import eos_tc_device_info
 
     execute_testcases.register(eos_tc_device_info)
 
+    # -------------------------------------------------------------------------
+    # Support the 'interfaces' testcases
+    # -------------------------------------------------------------------------
+
     from .eos_tc_interfaces import eos_tc_interfaces
 
     execute_testcases.register(eos_tc_interfaces)
+
+    # -------------------------------------------------------------------------
+    # Support the 'transceivers' testcases
+    # -------------------------------------------------------------------------
+
+    from .eos_tc_transceivers import eos_test_transceivers
+
+    execute_testcases.register(eos_test_transceivers)
---
+++ netcam_aio_devices/eos/eos_xcvr_matching.py
@@ -0,0 +1,28 @@
+__all__ = ["eos_xcvr_type_matches", "eos_xcvr_model_matches"]
+
+
+ARISTA_TYPE_ALIAS = {"100GBASE-AR4": "100GBASE-LR", "10GBASE-AR": "10GBASE-LR"}
+
+
+def eos_xcvr_model_matches(expected: str, measured: str) -> bool:
+
+    # do not take the lenth value in consideration.
+
+    if expected.startswith("AOC-S-S-10G"):
+        return measured.startswith("AOC-S-S-10G")
+
+    # -AR indicates an Arista vendor optic, so do not use this part in the
+    # comparison.
+
+    if measured.endswith("-AR"):
+        measured = measured.split("-AR", 1)[0]
+
+    return expected == measured
+
+
+def eos_xcvr_type_matches(expected: str, measured: str) -> bool:
+
+    if type_alias := ARISTA_TYPE_ALIAS.get(measured):
+        measured = type_alias
+
+    return expected == measured
